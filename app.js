318709c1fbec8b65ac81f8d301c1c6c24733c1b0
hituraj <hitesh.programmer83@gmail.com>
2020-08-20 17:34:11 +0530

    https://github.com/hituraj/typescripts.git

---
+++ app.js
@@ -0,0 +1,2 @@
+//https://www.youtube.com/watch?v=BwuLxPH8IDs&t=2001s
+console.log('Here I am available to learn the TypeScripts!! woo la la :)');
---
+++ app.ts
@@ -0,0 +1,3 @@
+//https://www.youtube.com/watch?v=BwuLxPH8IDs&t=2001s
+
+console.log('Here I am available to learn the TypeScripts!! woo la la :)');
